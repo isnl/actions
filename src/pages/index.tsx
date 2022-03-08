5a7a87d6776e7787a5d0b5dfac2e8e23db3bc7f3
Peanut <htmlcs@163.com>
2022-03-08 23:24:16 +0800

    fix: npm源改为淘宝源

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './index.less';
 
 export default function IndexPage() {
+  const [count, setCount] = useState(0);
   useEffect(() => {
     document.title = '标题';
     console.log('coming------');
   }, []);
   return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-      <div>标题-Github Actions自动部署</div>
+    <div className={styles.index}>
+      <h1 className={styles.title}>标题-Github Actions自动部署</h1>
+      <h2 onClick={() => setCount((val) => val + 1)}>{count}</h2>
     </div>
   );
 }
